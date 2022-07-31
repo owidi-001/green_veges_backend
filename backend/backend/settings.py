712110787f2ed473dbdd4430adbb929bc7030908
owidi-001 <kevinalex846@gmail.com>
2022-07-30 23:08:10 -0400

    deploy debug, secret key add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config.SECRET_KEY
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
