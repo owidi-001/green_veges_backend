9e3a8a06bff067e73766c20179acb0263e13d53e
owidi-001 <kevinalex846@gmail.com>
2022-08-18 02:19:07 -0400

    vendor template intergration

--- backend/vendor/urls.py
+++ backend/vendor/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import VendorViews
+from .views import dashboard_login,dashboard_register,dashboard_analytics,dashboard_products
 
 urlpatterns = [
-    path("vendor/", VendorViews.as_view(), name="vendor"),
+    path("vendor/", dashboard_login, name="login"),
+    path("vendor/create", dashboard_register, name="register"),
+    path("vendor/analytics", dashboard_analytics, name="analytics"),
+    path("vendor/products", dashboard_products, name="products"),
+    # path("vendor/", VendorViews.as_view(), name="vendor"),
 ]
--- backend/vendor/views.py
+++ backend/vendor/views.py
@@ -1,15 +1,33 @@
-from django.shortcuts import get_object_or_404
-from product.forms import ProductForm
-from product.forms import ProductUpdateForm
+from django.shortcuts import get_object_or_404, render
 from product.models import Product
 from product.schema import ProductSchema
 from product.serializer import ProductSerializer
-from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from vendor.models import Vendor
+
+
+# vendor dashboard
+def dashboard_login(request):
+    return render(request, "vendor/dashboard-login.html", {"title": "Vendor dashboard login"})
+
+
+def dashboard_register(request):
+    return render(request, "vendor/dashboard-register.html", {"title": "Vendor dashboard register"})
+
+
+def dashboard_analytics(request):
+    return render(request, "vendor/dashboard-analytics.html", {"title": "Vendor dashboard analytics"})
+
+
+def dashboard_products(request):
+    vendor = get_object_or_404(Vendor, user=request.user)
+    products = Product.objects.filter(vendor=vendor)
+
+    return render(request, "vendor/dashboard-products.html",
+                  {"title": "Vendor dashboard product", "products": products})
 
 
 class VendorViews(APIView):
